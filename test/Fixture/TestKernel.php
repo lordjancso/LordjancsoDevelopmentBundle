17206769c18d0ba6a4ebed76749015846ab3725b
lordjancso <lordjancso@gmail.com>
2016-02-29 14:40:35 +0100

    phpunit added

---
+++ test/Fixture/TestKernel.php
@@ -0,0 +1,25 @@
+<?php
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class TestKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Lordjancso\DevelopmentBundle\LordjancsoDevelopmentBundle(),
+        );
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config.yml');
+    }
+}
