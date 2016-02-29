bcfeae533b2a3f7e757d94cf3d041fa9b768593b
lordjancso <lordjancso@gmail.com>
2016-02-29 11:55:56 +0100

    CS fix command

---
+++ Command/CsFixerCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Lordjancso\DevelopmentBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class CsFixerCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('lordjancso:cs-fixer')
+            ->setDescription('Fix Symfony coding standards');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $process = new Process('php vendor/fabpot/php-cs-fixer/php-cs-fixer --dry-run --diff --verbose --config=sf23 --level=symfony fix ./');
+        $process->run();
+
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        $processOutput = $process->getOutput();
+
+        $output->writeln($processOutput);
+    }
+}
