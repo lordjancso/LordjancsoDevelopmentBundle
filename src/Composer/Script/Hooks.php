2bc948e924ceaa80b12c856034a18a16ffad0021
lordjancso <lordjancso@gmail.com>
2016-03-31 17:42:05 +0200

    Hook added

--- src/Composer/Script/Hooks.php
+++ src/Composer/Script/Hooks.php
@@ -8,14 +8,13 @@ class Hooks
 {
     public static function checkHooks(Event $event)
     {
-        $pwd = exec('pwd');
         $io = $event->getIO();
-        $gitHook = @file_get_contents($pwd.'/.git/hooks/pre-commit');
+        $gitHook = @file_get_contents(__DIR__.'/../../../../../../.git/hooks/pre-commit');
         $docHook = @file_get_contents(__DIR__.'/../../../docs/hooks/pre-commit');
 
         if ($gitHook !== $docHook) {
-            $io->write('<error>Please set up your hooks!</error>');
-            $io->write('<error>rm -rf .git/hooks && ln -s vendor/lordjancso/development-bundle/docs/hooks .git/hooks</error>');
+            $io->write('<error>Please set up your fucking hooks!</error>');
+            $io->write('<error>rm -rf .git/hooks && ln -s ../vendor/lordjancso/development-bundle/docs/hooks .git/hooks</error>');
 
             return false;
         }
