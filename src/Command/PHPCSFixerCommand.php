02963f7c3ea4654c2f828530033cf8d807c4caa7
lordjancso <lordjancso@gmail.com>
2016-06-21 15:01:25 +0200

    Disable dry run option added

--- src/Command/PHPCSFixerCommand.php
+++ src/Command/PHPCSFixerCommand.php
@@ -4,6 +4,7 @@ namespace Lordjancso\DevelopmentBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
@@ -12,14 +13,21 @@ class PHPCSFixerCommand extends Command
     protected function configure()
     {
         $this->setName('lordjancso:php-cs-fixer')
-            ->setDescription('PHP CS Fixer');
+            ->setDescription('PHP CS Fixer')
+            ->addOption('disable-dry-run', null, InputOption::VALUE_NONE, 'Disable dry run');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('PHP CS FIXER');
 
-        $command = new Process('php vendor/fabpot/php-cs-fixer/php-cs-fixer --dry-run --diff --verbose --config=sf23 --level=symfony --fixers=long_array_syntax,ordered_use fix ./');
+        $cmd = 'php vendor/fabpot/php-cs-fixer/php-cs-fixer';
+        if (!$input->getOption('disable-dry-run')) {
+            $cmd .= ' --dry-run';
+        }
+        $cmd .= ' --diff --verbose --config=sf23 --level=symfony --fixers=long_array_syntax,ordered_use fix ./';
+
+        $command = new Process($cmd);
         $command->setTimeout(null);
         $command->run(function ($type, $buffer) {
             echo $buffer;
